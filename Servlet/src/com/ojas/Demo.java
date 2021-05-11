f9921c85b0e7935e4dc1bc5a1d2d22b6bccae37a
Rasheeda_Vetapalem <v.rasheeda14@gmail.com>
2021-05-11 17:15:21 +0530

    Servlet

---
+++ Servlet/src/com/ojas/Demo.java
@@ -0,0 +1,51 @@
+package com.ojas;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Demo
+ */
+@WebServlet("/Demo")
+public class Demo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Demo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		
+		response.setContentType("text/html");
+		PrintWriter pw= response.getWriter();
+		String uname=request.getParameter("uname");
+		int age=Integer.parseInt(request.getParameter("age"));
+		if(age>18){
+			pw.println("<h1>Welcome to fashon show</h1>");
+		}
+		else
+			pw.println("<h1> welcome to cartoon show</h1>");
+	}
+
+}
